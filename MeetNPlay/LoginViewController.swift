dd728b25461f047474c14fb7863e26287c26f03f
vijayms <vijaymurrugappan@gmail.com>
2018-06-01 21:04:49 -0700

    Design of Login and Sign Up - 1

--- MeetNPlay/LoginViewController.swift
+++ MeetNPlay/LoginViewController.swift
@@ -9,21 +9,66 @@
 import UIKit
 import Firebase
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController,UITextFieldDelegate {
 
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    
+    // keyboard resigned when touched outside of the text field
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        scrollView.endEditing(true)
+        view.endEditing(true)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x: 0,y: 200) , animated: true) // moving keyboard up when text field did begin editing
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x: 0,y: 0) , animated: true)  // moving keyboard back down when text field did end editing
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        userTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        return true
     }
 
     @IBAction func loginClicked(_ sender: UIButton) {
+        if userTextField.text == "" || passwordTextField.text == "" {
+            showAlert(Title: "INVALID", Desc: "Username or Password not found")
+            return
+        }
         Auth.auth().signIn(withEmail: userTextField.text!, password: passwordTextField.text!, completion: {
             (user,error) in
-            self.performSegue(withIdentifier: "profile", sender: self)
+            if (error == nil) {
+                self.performSegue(withIdentifier: "profile", sender: self)
+                return
+            }
+            self.showAlert(Title: "INCORRECT", Desc: "Username or Password Incorrect")
         })
+    }
+    
+    func showAlert(Title: String, Desc: String) {
+        let alertController = UIAlertController(title: Title, message: Desc, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default) {
+            (UIAlertAction) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true, completion: nil)
     }
 
 }
---
+++ MeetNPlay/SignUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpViewController.swift
+//  MeetNPlay
+//
+//  Created by Vijay Murugappan Subbiah on 6/1/18.
+//  Copyright © 2018 VMS. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
